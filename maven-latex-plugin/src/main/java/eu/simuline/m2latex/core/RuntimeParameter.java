42438197610b854c060e5b6ef5a256953dbf9640
Ernst Reissner <ernst.reissner@simuline.eu>
2024-07-29 03:48:03 +0200

    extension: filtering of methods not only fields in injections applied to latexmkrc - annotation RuntimeParameters: rework, most important: applicable to methods now. - class Settings   - generalizing PATTERN_CONFIG holding now optional '()' after the name     indicating method rather than field if present.   - since now besides name a second group for the `()`,     turned numbered groups into named ones     adding according static fields.   - added several getter methods for command names   - endowed almost all with annotation @RuntimeParameter   - added method getMethodsNoParams() which refers to methods     as getProperties refers to fields.   - extended filterInjection to take besides fields which are parameters     also methods which are getters into account.

--- maven-latex-plugin/src/main/java/eu/simuline/m2latex/core/RuntimeParameter.java
+++ maven-latex-plugin/src/main/java/eu/simuline/m2latex/core/RuntimeParameter.java
@@ -1,18 +1,33 @@
 package eu.simuline.m2latex.core;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.apache.maven.plugins.annotations.Mojo;
 
 import org.apache.maven.plugins.annotations.Parameter;
 
 /**
- * Set whereever {@link Parameter} is set (in future automatically), 
- * to have a marker even at runtime. 
+ * For fields 
+ * set whereever {@link Parameter} is set (in future automatically), 
+ * to have a marker for parameters even at runtime. 
+ * Parameters are private member fields. 
  * Note that {@link Parameter} itself is not visible at runtime. 
+ * To generalize, 
+ * it is also possible to endow methods without parameter with this annotation. 
+ * That way, data can be read if statically available (fields)
+ * or dynamically computed (methods). 
+ * This annotation is used in {@link Mojo}s and in {@link Setting}s. 
+ * For use of {@link RuntimeParameter}s see {@link Setting#getProperties()}. 
+ * Besides for {@link Setting#toString()} 
+ * which use fields only, they go into injections by {@link Setting#getProperties()}. 
  */
 @Retention(value=RetentionPolicy.RUNTIME)
-@Documented
-public @interface RuntimeParameter {
+@Documented // to make the annotation occur in api docs 
+@Target({ElementType.FIELD, ElementType.METHOD})
+//public 
+@interface RuntimeParameter {
   // marker annotation 
 }
