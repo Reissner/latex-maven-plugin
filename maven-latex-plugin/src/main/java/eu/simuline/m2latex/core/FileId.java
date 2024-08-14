5ff398055eab2806fe9455262db84c73d804dad6
Ernst Reissner <ernst.reissner@simuline.eu>
2024-08-14 21:42:28 +0200

    Rework on class FileId FileId is the identifier of a file: equal files yield equal identifiers. Take only relevant lines of the file into account: those read by the auxiliary program. This avoids superfluous reruns. - replaced field length which was overall number of lines of the file   by number of relevant lines numLines, i.e. relevant for the Auxilary passed to the constructor. - also compute the (field) hash only over the relevant lines. - separated algorithm for hash into field ALGORITHM.

--- maven-latex-plugin/src/main/java/eu/simuline/m2latex/core/FileId.java
+++ maven-latex-plugin/src/main/java/eu/simuline/m2latex/core/FileId.java
@@ -1,65 +1,90 @@
 package eu.simuline.m2latex.core;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Provides an identifier for a file consisting of the size and a hash. 
- * We used md5. 
- * This is used to decide whether a file changed 
- * without storing the complete old file. 
- * This is done by a hash: a function that assigns an identifier to a file. 
- * Of course, there must be 'collisions', 
- * i.e. cases where different files have the same identifier, 
- * unless it is at least the file itself that serves as an identifier, 
- * but this is quite unlikely and we accept that in these rare cases, 
- * the identification fails. 
+ * Provides an immutable identifier for a file 
+ * consisting of the number of relevant lines lines 
+ * and a hash computed from these lines. 
+ * The the hash is computed with fixed algorithm {@link #ALGORITHM}. 
  * <p>
- * This class is applied in rerun file check, i.e. to trigger the run of a tool 
+ * The relevant parts of a file is given by an {@link #Auxiliary}, 
+ * So, the constructor has signature {@link #FileId(File, Auxiliary)}. 
+ * <p>
+ * If two files coincide in their relevant lines, 
+ * their {@link FileId}s coincide, 
+ * i.e. they are equal according to {@link #equals(Object)}. 
+ * If they don't coincide, then it is conceivable but still unlikely, 
+ * that their {@link FileId}s coincide. 
+ * We call this a collision. 
+ * So the {@link FileId} may be used to detect file changes quite safely. 
+ * <p>
+ * This class is applied in rerun file check, 
+ * i.e. to trigger the run of a tool 
  * if a file changed. 
  * In case of a collision, 
  * the execution of the tool is not triggered although needed. 
  */
 public class FileId {
 
-  final long length;
-  final String hash;
+  /**
+   * The algorithm used to compute the hash {@link #hash} 
+   * from lines of a file determined by an {@link Auxiliary}. 
+   * 
+   * @see #FileId(File, Auxiliary)
+   */
+  private static final String ALGORITHM = "MD5";
+
+  /**
+   * The number of lines written in a file relevant for the {@link Auxiliary} 
+   * given by the constructor {@link #FileId(File, Auxiliary)}. 
+   */
+  private final int numLines;
+
+  /**
+   * The hash of lines the number of which is given by {@link #numLines}. 
+   */
+  private final String hash;
 
   FileId(File file, Auxiliary aux) {
     assert !file.isDirectory();
-    this.length = file.length();
 
     try {
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      if (aux.update(file, md)) {
+      MessageDigest md = MessageDigest.getInstance(ALGORITHM);
+      // The number of relevant lines decided by method Auxiliary.update(...) 
+      AtomicInteger numLines = new AtomicInteger(0);
+      if (aux.update(file, md, numLines)) {
         this.hash = new String(md.digest());
+        this.numLines = numLines.get();
       } else {
         // TBD: add warning 
         System.out.println("EMPTY HASH IO");
         this.hash = "";
+        this.numLines = 0;
       }
     } catch (NoSuchAlgorithmException nsae) {
-      // TBD: emit warning 
-      throw new IllegalStateException("Algorithm should be known. ");
+      throw new IllegalStateException(
+          "Algorithm " + ALGORITHM + " should be known. ");
     }
   }
 
+  /**
+   * The given object equals this one if it is an instance of {@link FileId} 
+   * and both fields are equal. 
+   */
   public boolean equals(Object obj) {
     if (!(obj instanceof FileId)) {
       return false;
     }
-    FileId other = (FileId)obj;
-    return this.length == other.length
-      && this.hash.equals(other.hash);
+    FileId other = (FileId) obj;
+    return this.numLines == other.numLines && this.hash.equals(other.hash);
   }
 
   public String toString() {
-    return this.hash + " " + this.length;
+    return this.hash + " " + this.numLines;
   }
 }
 
